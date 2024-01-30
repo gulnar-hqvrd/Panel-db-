83b6c5a5eff3fb648a75bb1d6c966cf82641e9f1
Gulnar Hagverdiyeva <glnrhagverdiyeva@gmail.com>
2024-01-30 23:01:59 +0400

    Initial commit

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 $(() => {
-    // npx json-server --watch db.json  --port 3000
-    const url = 'http://localhost:3000/categories'
-    function loadCategories() {
-        $.getJSON(url, (data) => {
-            if (data.length > 0) {
+  // npx json-server --watch db.json  --port 3000
+  const url = "http://localhost:3000/categories";
+  function loadCategories(id) {
+    $.getJSON(`${url}?MasterId=${id}`, (data) => {
+      if (data.length > 0) {
+        let templates = [];
+        let count = $(".n-list").length; //0
 
-                let templates = [];
-                let count = $('.n-list').length; //0 
+        templates.push(
+          `<div class='col-3'>
+                            <ul class='n-list list-group list-group-flush border rounded shadow-sm p-3' data-order='${count}'> `
+        );
 
-                templates.push(
-                    `<div class='col-3'>
-                    <ul class='n-list list-group list-group-flush border rounded shadow-sm p-3' data-order='${count}'> `);
+        $.each(data, (i, item) => {
+          let template = `
+                            <li class='list-group-item main-li'> 
+                                <a class='categories' href='#' data-id='${item.id}'> ${item.CategoryName} </a> 
+                                <a class='btn btn-sm float-right delete shadow-sm' data-id='${item.id}'> 
+                                    <i class='fa fa-trash text-danger'> </i>
+                                <a/>
+                            </li>`;
+          templates.push(template);
+        });
 
-                $.each(data, (i, item) => {
+        templates.push("</ul></div>");
+        $(".n-menu").html(templates.join(""));
 
-                    let template = `
-                    <li class='list-group-item main-li'> 
-                        <a class='categories' href='#' data-id='${item.id}'> ${item.CategoryName} </a> 
-                        <a class='btn btn-sm float-right delete shadow-sm' data-id='${item.id}'> 
-                            <i class='fa fa-trash text-danger'> </i>
-                        <a/>
-                    </li>`;
-                    templates.push(template);
-                });
+        // appent +=
+        // html   =
+      }
+    }).fail((err) => {
+      console.log(err.message);
+    });
+  }
+  loadCategories("null");
 
-                templates.push('</ul></div>');
-                $('.n-menu').html(templates.join(''));
+  //   $(".categories").click(function () {
+  //     alert("zxzsad");
+  //   });
 
-                // appent +=
-                // html   =
-            }
-        }).fail((err) => { console.log(err.message) });
-    }
+//   $(".n-menu").on("click", ".categories", function () {
+//     alert("zxzsad");
+//   });
 
 
-    loadCategories();
-})
+});
