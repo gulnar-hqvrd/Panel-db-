d4f21ca331392392689b26c9138be9afd20b2128
Gulnar Hagverdiyeva <glnrhagverdiyeva@gmail.com>
2024-02-02 18:28:11 +0400

    Initial commit

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ $(() => {
 
         templates.push(
           `<div class='col-3'>
-                            <ul class='n-list list-group list-group-flush border rounded shadow-sm p-3' data-order='${count}'> `
+                  <ul class='n-list list-group list-group-flush border rounded shadow-sm p-3' data-order='${count}'> `
         );
 
         $.each(data, (i, item) => {
           let template = `
-                            <li class='list-group-item main-li'> 
-                                <a class='categories' href='#' data-id='${item.id}'> ${item.CategoryName} </a> 
-                                <a class='btn btn-sm float-right delete shadow-sm' data-id='${item.id}'> 
-                                    <i class='fa fa-trash text-danger'> </i>
-                                <a/>
-                            </li>`;
+                  <li class='list-group-item main-li'> 
+                      <a class='categories' href='#' data-id='${item.id}'> ${item.CategoryName} </a> 
+                      <a class='btn btn-sm float-right delete shadow-sm' 
+                      data-id='${item.id}'
+                      data-adim="murat"
+                      data-soyadim="vuranok"
+                      data-mail="murat.vuranok@code.edu.az" >  <i class='fa fa-trash text-danger'> </i>
+                      <a/>
+                  </li>`;
           templates.push(template);
         });
 
@@ -35,13 +38,106 @@ $(() => {
   }
   loadCategories("null");
 
-  //   $(".categories").click(function () {
-  //     alert("zxzsad");
-  //   });
+  // // $('.n-list').on('click', '.categories', function () {
+  // //     alert('sd')
+  // // });
 
-//   $(".n-menu").on("click", ".categories", function () {
-//     alert("zxzsad");
-//   });
+  // // $('.categories').click(function () { alert('zxzsad') });
+  // $(document).on('click', '.categories', () => { alert('deneme') });
 
+  $(document).on("click", ".categories", (e) => {
+    // let id = e.currentTarget.dataset.id;
+    let item = e.currentTarget;
+    $("#cname").text(item.innerText);
 
+    let id = item.dataset.id;
+    $("#MasterId").val(id);
+  });
+
+  // let menuList = document.querySelector('.n-menu');
+  // menuList.addEventListener('click', (e) => {
+
+  //     if (e.target.classList.contains('categories')) {
+  //         let id = e.target.dataset.id;
+
+  //         alert(id)
+  //     }
+  // })
+
+  $("#btnKaydet").click(() => {
+    let categoryName = $("#CategoryName").val();
+    let masterId = $("#MasterId").val();
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      dataType: "json",
+      contentType: "application/json",
+      data: JSON.stringify({
+        CategoryName: categoryName,
+        MasterId: masterId,
+      }),
+      success: (response) => {
+        document.getElementById("currentForm").reset();
+        document.getElementById("MasterId").value = "null";
+        loadCategories("null");
+      },
+      error: (err) => {
+        console.log(err.message);
+      },
+    });
+  });
+
+  /**
+   * Delete Function
+   */
+  $(document).on("click", ".delete", (e) => {
+    let currentItem = e.currentTarget;
+    let id = $(currentItem).data("id");
+
+    fetch(`${url}/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      let li = $(currentItem).closest("li");
+      $(li).fadeOut(300, () => {
+        let parent = $(li).parent();
+        $(li).remove();
+        let liCount = $(".main-li").length;
+        if (liCount == 0) {
+          $(parent).remove();
+        }
+      });
+    });
+  });
+
+  // document.getElementById('btnKaydet1').addEventListener('click', () => {
+
+  //     let categoryName = document.getElementById('CategoryName').value;
+  //     let masterId = document.getElementById('MasterId').value;
+
+  //     fetch(url, {
+  //         method: 'POST',
+  //         headers: {
+  //             'Content-Type': 'application/json',
+  //         },
+  //         body: JSON.stringify({
+  //             CategoryName: categoryName,
+  //             MasterId: masterId
+  //         })
+  //     })
+  //         .then(response => {
+  //             if (!response.ok) {
+  //                 throw new Error('Network response was not ok');
+  //             }
+  //             return response.json();
+  //         })
+  //         .then(data => {
+  //             console.log(data);
+  //             var form = document.getElementById('currentForm');
+  //             form.reset();
+  //         })
+  //         .catch(error => {
+  //             console.error(error.message);
+  //         });
+  // });
 });
